c328bcc55fe22f31811757a25091806a712f08b8
na0ha <gloryna0@naver.com>
2019-05-14 13:36:50 +0900

    updated

--- 하나영/개발호재 코드.R
+++ 하나영/개발호재 코드.R
@@ -1,31 +1,32 @@
 rm(list=ls())
 getwd()
-setwd("C:\\Users\\user\\Desktop\\Project_apartment\\정동호")
+setwd("C:\\Users\\user\\Desktop\\Project_apartment")
 #dir() 파일명 
-dat = read.csv("단지별변수(수정).csv")
+dat = read.csv("단지별변수(추가).csv")
 
 #View(dat)
 #str(dat)
 dat$읍면동 = as.character(dat$읍면동)
 loc = dat$읍면동 
 #loc
-dangha = grep('당하동', loc)
-wondang = grep('원당동', loc)
-bulo = grep('불로대곡동' , loc)
-songdo=grep('송도동', loc)
+dangha = grep('당하', loc)
+wondang = grep('원당', loc)
+bulo = grep('불로대곡' , loc)
+songdo=grep('송도', loc)
 
-eunseo=grep('운서동', loc)
-youngjong=grep('영종동',loc)
-dohwa=grep('도화동',loc)
-soongee=grep('숭의동',loc)
-chungla=grep('청라동', loc)
-suknam=grep('석남동',loc)
-gaza=grep('가좌동',loc)
-gajung=grep('가정동',loc)
-gumdan=grep('검단동',loc)
-bupyeong=grep('부평동',loc)
-hwasu = grep('화수동', loc) 
-songlim=grep('송림동',loc)
+eunseo=grep('운서', loc)
+youngjong=grep('영종',loc)
+dohwa=grep('도화',loc)
+soongee=grep('숭의',loc)
+chungla=grep('청라', loc) 
+gumam=grep('검암',loc)
+suknam=grep('석남',loc)
+gaza=grep('가좌',loc)
+gajung=grep('가정',loc)
+gumdan=grep('검단',loc)
+bupyeong=grep('부평',loc)
+hwasu = grep('화수', loc) 
+songlim=grep('송림',loc)
 
 #dep=c("dangha","wondang","bulo","songdo","eunseo",
 #   "youngjong","dohwa","soongee","chungla",
@@ -47,6 +48,7 @@ dat[youngjong,]$개발호재유무 = 1
 dat[dohwa,]$개발호재유무 = 1
 dat[soongee,]$개발호재유무 = 1
 dat[chungla,]$개발호재유무 =1
+dat[gumam,]$개발호재유무=1
 dat[suknam,]$개발호재유무 = 1
 dat[gaza,]$개발호재유무 = 1
 dat[gajung,]$개발호재유무 =1
@@ -58,5 +60,5 @@ dat[songlim,]$개발호재유무 = 1
 dat[which(is.na(dat$개발호재유무)),]$개발호재유무<-0
 
 View(dat)
-write.csv(dat,'단지별변수(추가).csv')
+write.csv(dat,'단지별변수(new).csv')
 
